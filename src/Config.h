da88930ad609dc7a37b982a7ce9533fd3ab070e2
val218 <105395725+val218@users.noreply.github.com>
2026-03-31 22:16:25 +0300

    Add files via upload

---
+++ src/Config.h
@@ -0,0 +1,18 @@
+#pragma once
+
+// Instead of editing this file, consider adding -D lines in platformio.ini
+
+// System Interface
+
+// #define ECHO_FNC_TO_DEBUG
+
+// #define UART_ON_PORT_B // Not recommended, see comment in System.h
+
+// Automatically go to Jog Scene when first connected
+// #define AUTO_JOG_SCENE
+
+// Automatically go to Homing Scene when unhomed alarm is present
+// #define AUTO_HOMING_SCENE
+
+// Automatically leave Homing Scene after homing is finished
+// #define AUTO_HOMING_RETURN
